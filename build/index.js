c0495b0896b10f75fbd5afa9a86e877a41835ae3
Pavan.Tikkani <pavan.tikkani@bioclinica.com>
2020-09-02 16:43:36 +0530

    Added more files

--- build/index.js
+++ build/index.js
@@ -5,12 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var body_parser_1 = __importDefault(require("body-parser"));
-// import cookieSession from "cookie-session";
+var cookie_session_1 = __importDefault(require("cookie-session"));
 var AppRouter_1 = require("./AppRouter");
 require("./controllers/RootController");
 var app = express_1.default();
 app.use(body_parser_1.default.urlencoded({ extended: true }));
-// app.use(cookieSession({keys: ['UU6D4b10StP2zvexJeoSsoGSRBBB7jhzzZqe3yH0y7vU7E']}))
+app.use(cookie_session_1.default({ keys: ['UU6D4b10StP2zvexJeoSsoGSRBBB7jhzzZqe3yH0y7vU7E'] }));
 app.use(AppRouter_1.AppRouter.getInstance());
 app.listen(3000, function () {
     console.log("Listening on port 3000");
